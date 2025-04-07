bd82cf32d42f28acdb1633cff0f61cca735428e7
Adan Yañez <adanyanezgonzalez@gmail.com>
2025-04-06 21:17:58 -0600

    remove save

--- src/infrastructure/persistence/file/UserRepositoryMemory.ts
+++ src/infrastructure/persistence/file/UserRepositoryMemory.ts
@@ -4,38 +4,28 @@ import { IUserRepository } from "@domain/interfaces/repositories/IUserRepository
 export class UserRepositoryMemory implements IUserRepository {
   private users: User[] = [];
 
-  findAll(): Promise<User[]> {
-    return Promise.resolve(this.users);
+  async findAll(): Promise<User[]> {
+    return this.users;
   }
-  findAllBy(filter: User): Promise<User[] | null> {
-    return Promise.resolve(
-      this.users.filter((user) => user.name === filter.name)
-    );
+  async findAllBy(filter: User): Promise<User[] | null> {
+    return this.users.filter((user) => user.name === filter.name);
   }
-  create(user: User): Promise<string> {
+  async create(user: User): Promise<string> {
     this.users.push(user);
-    return Promise.resolve(user.id);
+    return user.id;
   }
-  update(user: User): Promise<User> {
+  async update(user: User): Promise<User> {
     const index = this.users.findIndex((u) => u.id === user.id);
     if (index !== -1) {
       this.users[index] = user;
     }
-    return Promise.resolve(user);
+    return user;
   }
-  delete(id: string): Promise<void> {
+  async delete(id: string): Promise<void> {
     this.users = this.users.filter((user) => user.id !== id);
-    return Promise.resolve();
   }
 
   async findById(id: string): Promise<User | null> {
     return this.users.find((user) => user.id === id) || null;
-  }
-
-  async save(user: User): Promise<void> {
-    const index = this.users.findIndex((u) => u.id === user.id);
-    if (index !== -1) {
-      this.users[index] = user;
-    }
   }
 }
