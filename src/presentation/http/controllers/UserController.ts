a84bb4e87c8aa408a71a0a0b5e7240ecad2b4ba0
Adan Yañez <adanyanezgonzalez@gmail.com>
2025-04-15 20:58:28 -0600

    basic user crud

--- src/presentation/http/controllers/UserController.ts
+++ src/presentation/http/controllers/UserController.ts
@@ -1,9 +1,6 @@
 import { User } from "@domain/entities/User";
 import { IRepository } from "@domain/interfaces/repositories/IRepository";
 import { HttpController } from "@infrastructure/http/HttpController";
-import { HttpMethod } from "@infrastructure/http/HttpMethod";
-import type { HttpRequest } from "@infrastructure/http/HttpRequest";
-import type { HttpResponse } from "@infrastructure/http/HttpResponse";
 
 /**
  * Controller that handles the requests related to users.
@@ -13,10 +10,4 @@ export class UserController extends HttpController<User> {
     super(repository);
   }
 
-  @HttpMethod("get", "/:name")
-  public async findOneByName(req: HttpRequest, res: HttpResponse): Promise<void> {
-    const id = await req.getParam("id");
-    const user = await this.repository.findById(id);
-    res.json(user);
-  }
 }
