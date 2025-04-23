878f275fae8d13bcd988c6fda3ce091fea2ea420
Adan Yañez <adanyanezgonzalez@gmail.com>
2025-04-22 20:02:57 -0600

    Add configs jest

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,25 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", {}],
-  },
-  moduleNameMapper: {
-    "^@application/(.*)$": "<rootDir>/src/application/$1",
-    "^@domain/(.*)$": "<rootDir>/src/domain/$1",
-    "^@infrastructure/(.*)$": "<rootDir>/src/infrastructure/$1",
-    "^@presentation/(.*)$": "<rootDir>/src/presentation/$1",
-  },
-  testMatch: ["**/__tests__/**/*.spec.ts", "**/?(*.)+(spec|test).ts"],
-  moduleFileExtensions: ["ts", "js", "json"],
-  clearMocks: true,
-  coverageDirectory: "coverage",
-  collectCoverage: true,
-  coveragePathIgnorePatterns: [
-    "<rootDir>/src/test/",
-    "\\.test\\.ts$", 
-  ]
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/src"],
+    transform: {
+        "^.+\\.tsx?$": ["ts-jest", {}],
+    },
+    moduleNameMapper: {
+        "^@application/(.*)$": "<rootDir>/src/application/$1",
+        "^@domain/(.*)$": "<rootDir>/src/domain/$1",
+        "^@infrastructure/(.*)$": "<rootDir>/src/infrastructure/$1",
+        "^@presentation/(.*)$": "<rootDir>/src/presentation/$1",
+    },
+    testMatch: ["**/__tests__/**/*.spec.ts", "**/?(*.)+(spec|test).ts"],
+    moduleFileExtensions: ["ts", "js", "json"],
+    clearMocks: true,
+    coverageDirectory: "coverage",
+    collectCoverage: true,
+    coveragePathIgnorePatterns: [
+        "<rootDir>/src/test/",
+        "\\.test\\.ts$",
+    ],
+    coverageReporters: ["text", "lcov"]
 };
